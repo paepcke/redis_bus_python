dd00aecae464744fdeac2dfd6a9a575a54a1580b
Andreas Paepcke <paepcke@cs.stanford.edu>
2015-08-18 17:36:14 -0700

    Added automatic subscription to tmp.* to engine demo, and corrected a number of engine part alt texts.

--- src/schoolbus_test_server/html/carEngine.js
+++ src/schoolbus_test_server/html/carEngine.js
@@ -6,28 +6,30 @@ var wireImageRegions = function() {
     areas  = carMap.areas;
     for (var i=0; i<areas.length; i++) {
 	//areas[i].addEventListener("click", areaClickHandler);
-	areas[i].addEventListener('click', function() {
-	    partName = areas[i].alt;
-	    handler = function(evt) {
+	areas[i].addEventListener('click', function(evt) {
 		// Don't follow the (random) URL
 		// associated with the area's src;
 		// We make a bus request instead:
 		evt.preventDefault();
+		
 		// If not subscribed to 'tmp.*' then
 		// subscribe temporarily:
 		wasTmpSubscribed = parent.window.sbTesterControl.subscribedTo('tmp.*');
 		if (! wasTmpSubscribed) {
 			parent.window.sbTesterControl.subscribeToTopic('tmp.*');
 		}
+		
+		// Area object user clicked on:
+		clickedImgArea = evt.currentTarget;
+		partName = clickedImgArea.alt;
+		
 		parent.window.sbTesterControl.sendReq(
 		    {"oneShot" : {"topic" : partName, "summary" : "1"},
 		     "oneShotTopic" : 'wikipedia'
 		    }
 		);
 		return false;
-	    }
-	    return handler;
-	}(), false);
+	    }, false);
     }
 }
 wireImageRegions();
