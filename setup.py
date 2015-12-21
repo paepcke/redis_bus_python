29c96849575896c3e6420dc4f6688666dab823fb
Andreas Paepcke <paepcke@cs.stanford.edu>
2015-12-21 09:52:22 -0800

    Corrected setup.py to find the packages to upload by adding arg 'src' to the find_package call. I'm hoping that this will cause pip install to pull the actual code from pypi.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ test_requirements = ['sentinels>=0.0.6', 'nose>=1.0', 'python-dateutil>=2.2']
 
 setup(
     name = "redis_bus_python",
-    version = "0.0.4",
-    packages = find_packages(),
+    version = "0.0.5",
+    packages = find_packages('src'),
 
     # Dependencies on other packages:
     setup_requires   = ['nose>=1.1.2'],
